09163219c61ad82150b24811cce3e5e6d6fc0043
OGurso <70133220+OGurso@users.noreply.github.com>
2021-03-02 14:03:39 +0100

    Added navbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
-
 import Admin from './Admin';
 import Error from './Error';
 import Landing from './Landing';
 import Member from './Member';
 import Navbar from './Navbar';
+import GlobalStyle from './GlobalStyle';
 
 function App() {
   return (
 
     <Router>
+      <GlobalStyle />
       <Navbar />
-    <div>
-      <Switch>
-      <Route path="/" exact component={Landing} />
-      <Route path="/navbar" exact component={Navbar} />
-      <Route path="/member" exact component={Member} />
-      <Route path="/admin" exact component={Admin} />
-       <Route component={Error} />
-      </Switch>
-    </div>
+      <div>
+        <Switch>
+          <Route path="/" exact component={Landing} />
+          <Route path="/navbar" exact component={Navbar} />
+          <Route path="/member" exact component={Member} />
+          <Route path="/admin" exact component={Admin} />
+          <Route component={Error} />
+        </Switch>
+      </div>
     </Router>
   );
 }
