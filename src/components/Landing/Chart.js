b48ac9a8537b5312b0b9457e653e8e0ddaf7da28
Fadil Al <fadil.alfarhan@gmail.com>
2021-03-16 13:19:23 +0100

    fixed algorithm for the ratesChart

--- src/components/Landing/Chart.js
+++ src/components/Landing/Chart.js
@@ -5,6 +5,10 @@ import { Line } from 'react-chartjs-2';
 const Chart = () => {
 
   const [chartData, setChartData] = useState([])
+  const [apiBase, setApiBase] = useState("&base=EUR")
+  const [sekChart, setSekChart] = useState([])
+  const [newData, setNewData] = useState([])
+  
 
   const options = {
     method: 'GET',
@@ -16,14 +20,30 @@ const Chart = () => {
     }
   };
 
-  const API_URL = 'http://data.fixer.io/api/2004-08-10?access_key=d11513f2fab75e1e6957748307f9f943&format=1'
+  const API_URL = 'https://api.exchangerate.host/timeseries?symbols=USD&start_date=2020-01-01&end_date=2020-07-01'
 
-useEffect(() => {
-  axios.request(API_URL)
-  .then(response => console.log(response.data))
-  .catch(error => console.error(error));
-},[]);
+  useEffect(() => {
+    axios.request(API_URL+apiBase)
+    .then(response => setChartData([response.data.rates]))
+    .catch(error => console.error(error));
+  },[]);
+  useEffect(()=>{
+    setNewData(chartData.reduce((prev, next)=>{
+      // console.log(Object.keys(next[0]).length)
+      // let dates = Object.entries(next).filter(dates=>dates[8]==0 && dates[9]==1)
+      let dates = Object.entries(next).filter(data=>data[0][8].includes('0') && data[0][9].includes('1') )
+      // let rates = Object.values(next)
+      
+      
+      // console.log(rates)
+      // if(dates[8] == 0 && dates[9] == 1){
 
+      // }
+      // prev[dates] = rates
+      return prev[dates]
+    },{}))
+
+  },[chartData])
 // useEffect(() => {
 //   console.log("från onurs fetch")
 //   fetch("http://data.fixer.io/api/2013-12-24?access_key=fd4a8fdd5b24307002c1bea2c63f14f0&GBP")
@@ -45,22 +65,21 @@ createGraphData() {
   }
 }
 */
+
+  chartData.map(data =>{ return data.rates }).map(data=>{ return data })
+    
+    
+
   const rateData = {
     labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'July', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'],
     datasets: [
-      {
-        label: 'SEK / EUR',
-        backgroundColor: '#ecbcfd5b',
-        borderColor: '#571d85',
-        borderWidth: 2,
-        data: [65, 59, 80, 81, 56, 59, 30, 50, 120, 23, 12, 80, 81, 56, 43,0]
-      },
       {
         label: 'SEK / USD',
         backgroundColor: '#c229294b',
         borderColor: '#c22929',
         borderWidth: 2,
         data: [12, 63, 32, 23, 63, 43, 40, 59, 80, 81, 56, 43,30]
+        // data: newData.length>0?newData:[12, 63, 32, 23, 63, 43, 40, 59, 80, 81, 56, 43,30]
       }
     ]
   }
@@ -71,7 +90,6 @@ createGraphData() {
             data={rateData}
             width={600}
             height={400}
-            
             options={{
                   title:{     
                         display:true,
@@ -82,8 +100,7 @@ createGraphData() {
                         display:true,
                         position:'top',
                         
-                  } 
-                
+                  }   
           }}
         />
       </div>
