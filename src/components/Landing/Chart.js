4283e15c219ee8f7c116fb5a8d12be4bb90e4099
Fadil Al <fadil.alfarhan@gmail.com>
2021-03-16 11:09:19 +0100

    Co-authored-by: kayzersozee <kayzersozee@users.noreply.github.com> Co-authored-by: NikitaWB <NikitaWB@users.noreply.github.com>

--- src/components/Landing/Chart.js
+++ src/components/Landing/Chart.js
@@ -1,10 +1,6 @@
-import {useEffect, useState} from 'react';
-import {Line} from 'react-chartjs-2';
 import axios from 'axios';
-
-
-
-
+import { useEffect, useState } from 'react';
+import { Line } from 'react-chartjs-2';
 
 const Chart = () => {
 
