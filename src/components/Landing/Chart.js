fe0dbc385fb12c5183c6f44acd51b2bd8749068c
NikitaWB <70134216+NikitaWB@users.noreply.github.com>
2021-03-18 09:12:32 +0100

    Trying to fix Chart

--- src/components/Landing/Chart.js
+++ src/components/Landing/Chart.js
@@ -1,102 +1,93 @@
-import axios from 'axios';
-import { useEffect, useState } from 'react';
-import { Line } from 'react-chartjs-2';
+import { useEffect, useState } from "react";
+import { Line } from "react-chartjs-2";
+import axios from "axios";
+import { rates } from "../../constants/rates";
 
 const Chart = () => {
+  const [chartData, setChartData] = useState([]);
+  const [apiBase, setApiBase] = useState("&base=EUR");
+  const [drawData, setDrawData] = useState([]);
 
-  const [chartData, setChartData] = useState([])
-  const [apiBase, setApiBase] = useState("&base=EUR")
-  const API_URL = 'https://api.exchangerate.host/timeseries?symbols=USD&start_date=2020-01-01&end_date=2020-07-01'
-
-  const options = {
-    method: 'GET',
-    url: 'https://fixer-fixer-currency-v1.p.rapidapi.com/2013-12-24',
-    params: {base: 'USD', symbols: 'USD,CAD,EUR'},
-    headers: {
-      'x-rapidapi-key': '125c7009c6mshbb789189dfc362bp1cee93jsn5a077262afba',
-      'x-rapidapi-host': 'fixer-fixer-currency-v1.p.rapidapi.com'
-    }
-  };
+  const ratesArr = rates;
 
   useEffect(() => {
-    axios.request(API_URL+apiBase)
-    .then(response => setChartData([response.data]))
-    .catch(error => console.error(error));
-  },[]);
+    setDrawData([ratesArr[0].rates]);
 
-  // Create a function drawChart
-  // filter the first day and get a obj with {symbol and value} in month push the value to an array
-  // map
-function drawChart() {
-  chartData.map((rates) => console.log(rates.rates))
- /*  console.log(chartData[0].rates) */
-}
-drawChart()
+    const newDrawData = (drawData[0])
+    console.log(newDrawData)
+  }, []);
+  console.log(drawData[0])
+ /*  console.log(Object.keys(newDrawData)); */
+  /*   const API_URL =
+    "https://api.exchangerate.host/timeseries?symbols=USD&start_date=2020-01-01&end_date=2020-07-01";
 
-// useEffect(() => {
-//   console.log("från onurs fetch")
-//   fetch("http://data.fixer.io/api/2013-12-24?access_key=fd4a8fdd5b24307002c1bea2c63f14f0&GBP")
-//   .then(response => response.json())
-//   .then(data => console.log(data))
-// }, [])
+  useEffect(() => {
+    axios
+      .request(API_URL + apiBase)
+      .then((response) => setChartData(response.data.rates))
+      .catch((error) => console.error(error));
+  }, []); */
 
-
-/*
-createGraphData() {
-  let data ={Jan: 65, Feb: 59, Mar: 56}
+  // useEffect(() => {
+  //   console.log("från onurs fetch")
+  //   fetch("http://data.fixer.io/api/2013-12-24?access_key=fd4a8fdd5b24307002c1bea2c63f14f0&GBP")
+  //   .then(response => response.json())
+  //   .then(data => console.log(data))
+  // }, [])
 
   const rateData = {
-    labels: Object.keys(data)
-    ...data
-    datasets: {
-      data: Object.values(data)
-    }
-  }
-}
-*/
-  const rateData = {
-    labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'July', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'],
+    labels: [
+      "Jan",
+      "Feb",
+      "Mar",
+      "Apr",
+      "May",
+      "June",
+      "July",
+      "Aug",
+      "Sept",
+      "Oct",
+      "Nov",
+      "Dec",
+    ],
     datasets: [
       {
-        label: 'SEK / EUR',
-        backgroundColor: '#ecbcfd5b',
-        borderColor: '#571d85',
+        label: "SEK / EUR",
+        backgroundColor: "#ecbcfd5b",
+        borderColor: "#571d85",
         borderWidth: 2,
-        data: [65, 59, 80, 81, 56, 59, 30, 50, 120, 23, 12, 80, 81, 56, 43,0]
+        data: [65, 59, 80, 81, 56, 59, 30, 50, 120, 23, 12, 80, 81, 56, 43, 0],
       },
       {
-        label: 'SEK / USD',
-        backgroundColor: '#c229294b',
-        borderColor: '#c22929',
+        label: "SEK / USD",
+        backgroundColor: "#c229294b",
+        borderColor: "#c22929",
         borderWidth: 2,
-        data: [12, 63, 32, 23, 63, 43, 40, 59, 80, 81, 56, 43,30]
-      }
-    ]
-  }
-  
-    return (
-      <div>
-        <Line
-            data={rateData}
-            width={600}
-            height={400}
-            
-            options={{
-                  title:{     
-                        display:true,
-                        text:'Fluctuations over time',
-                        fontSize:14
-                  },
-                  legend:{
-                        display:true,
-                        position:'top',
-                        
-                  } 
-                
-          }}
-        />
-      </div>
-    );
-  }
+        data: [12, 63, 32, 23, 63, 43, 40, 59, 80, 81, 56, 43, 30],
+      },
+    ],
+  };
 
-export default Chart
+  return (
+    <div>
+      <Line
+        data={rateData}
+        width={600}
+        height={400}
+        options={{
+          title: {
+            display: true,
+            text: "Fluctuations over time",
+            fontSize: 14,
+          },
+          legend: {
+            display: true,
+            position: "top",
+          },
+        }}
+      />
+    </div>
+  );
+};
+
+export default Chart;
