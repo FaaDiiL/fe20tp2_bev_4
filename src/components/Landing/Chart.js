eec4cf89a2b58b2cf71e68bc54a90549e86351ae
kayzersozee <63726574+kayzersozee@users.noreply.github.com>
2021-03-18 12:27:12 +0100

    functionality to dynamic chart added

--- src/components/Landing/Chart.js
+++ src/components/Landing/Chart.js
@@ -1,83 +1,58 @@
-import {useEffect, useState} from 'react';
-import {Line} from 'react-chartjs-2';
-import axios from 'axios';
-
-
-
-
+import { useEffect, useState } from "react";
+import { Line } from "react-chartjs-2";
+import axios from "axios";
+import { rates } from "../../constants/rates";
+import { StayCurrentLandscapeOutlined } from "@material-ui/icons";
 
 const Chart = () => {
+  const [apiBase, setApiBase] = useState("&base=EUR");
 
-  const [chartData, setChartData] = useState([])
+  const ratesData = Object.entries(rates[0].rates);
 
-  const options = {
-    method: 'GET',
-    url: 'https://fixer-fixer-currency-v1.p.rapidapi.com/2013-12-24',
-    params: {base: 'USD', symbols: 'USD,CAD,EUR'},
-    headers: {
-      'x-rapidapi-key': '125c7009c6mshbb789189dfc362bp1cee93jsn5a077262afba',
-      'x-rapidapi-host': 'fixer-fixer-currency-v1.p.rapidapi.com'
+  let dates = [];
+  let rateOfDate = [];
+
+  let i;
+  for (i = 0; i < ratesData.length; i++) {
+    if (ratesData[i][0].endsWith("01")) {
+      dates.push(ratesData[i][0]);
+      rateOfDate.push(ratesData[i][1].USD);
     }
-  };
-
-  const API_URL = 'http://data.fixer.io/api/2004-08-10?access_key=d11513f2fab75e1e6957748307f9f943&format=1'
-
-useEffect(() => {
-  axios.request(API_URL)
-  .then(response => console.log(response.data))
-  .catch(error => console.error(error));
-},[]);
-
-// useEffect(() => {
-//   console.log("från onurs fetch")
-//   fetch("http://data.fixer.io/api/2013-12-24?access_key=fd4a8fdd5b24307002c1bea2c63f14f0&GBP")
-//   .then(response => response.json())
-//   .then(data => console.log(data))
-// }, [])
+  }
 
   const rateData = {
-    labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'July', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'],
+    labels: dates,
     datasets: [
       {
-        label: 'SEK / EUR',
-        backgroundColor: '#ecbcfd5b',
-        borderColor: '#571d85',
+        label: "SEK / USD",
+        backgroundColor: "#ecbcfd5b",
+        borderColor: "#571d85",
         borderWidth: 2,
-        data: [65, 59, 80, 81, 56, 59, 30, 50, 120, 23, 12, 80, 81, 56, 43,0]
+        data: rateOfDate,
       },
-      {
-        label: 'SEK / USD',
-        backgroundColor: '#c229294b',
-        borderColor: '#c22929',
-        borderWidth: 2,
-        data: [12, 63, 32, 23, 63, 43, 40, 59, 80, 81, 56, 43,30]
-      }
-    ]
-  }
-  
-    return (
-      <div>
-        <Line
-            data={rateData}
-            width={600}
-            height={400}
-            
-            options={{
-                  title:{     
-                        display:true,
-                        text:'Fluctuations over time',
-                        fontSize:14
-                  },
-                  legend:{
-                        display:true,
-                        position:'top',
-                        
-                  } 
-                
-          }}
-        />
-      </div>
-    );
-  }
+    ],
+  };
 
-export default Chart
+  return (
+    <div>
+      <Line
+        data={rateData}
+        width={600}
+        height={400}
+        options={{
+          title: {
+            display: true,
+            text: "Fluctuations over time",
+            fontSize: 14,
+          },
+          legend: {
+            display: true,
+            position: "top",
+          },
+        }}
+      />
+    </div>
+  );
+};
+
+export default Chart;
