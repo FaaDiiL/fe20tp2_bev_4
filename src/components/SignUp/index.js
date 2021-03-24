969e9520fdcb8bd32eeb3dcfba4d3177cba80c5f
Nonis17 <emma.lilja@student.kyh.se>
2021-03-24 14:48:30 +0100

    Rm compile warnings

--- src/components/SignUp/index.js
+++ src/components/SignUp/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Link, withRouter } from "react-router-dom";
+import { withRouter } from "react-router-dom";
 import styled from 'styled-components'
 
 import * as ROLES from "../../constants/roles";
