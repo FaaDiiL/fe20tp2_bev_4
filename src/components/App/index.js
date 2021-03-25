b0273b0390faa483ea26e1cb5dcc880e6b5258c0
Fadil Al <fadil.alfarhan@gmail.com>
2021-03-25 10:48:56 +0100

    formating....

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,35 +1,41 @@
-import React from "react";
-import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
+import React from 'react'
+import { Route, BrowserRouter as Router, Switch } from 'react-router-dom'
 
-import * as ROUTES from "../../constants/routes";
+import * as ROUTES from '../../constants/routes'
 import GlobalStyle, { GlobalStyleLF, GlobalStyleSB } from '../../GlobalStyle'
-import About from '../About';
-import AccountPage from "../Account";
-import AdminPage from "../Admin";
-import ContactPage from "../Contact";
-import Error from '../Error';
-import HomePage from "../Home";
-import LandingPage from '../Landing';
-import Navbar from "../Navbar"
-import PasswordForgetPage from "../PasswordForget";
-import { withAuthentication } from "../Session";
-import { AuthUserContext } from "../Session";
-import SignInPage from "../SignIn";
-import SignUpPage from "../SignUp";
+import About from '../About'
+import AccountPage from '../Account'
+import AdminPage from '../Admin'
+import ContactPage from '../Contact'
+import Error from '../Error'
+import HomePage from '../Home'
+import LandingPage from '../Landing'
+import Navbar from '../Navbar'
+import PasswordForgetPage from '../PasswordForget'
+import { withAuthentication } from '../Session'
+import { AuthUserContext } from '../Session'
+import SignInPage from '../SignIn'
+import SignUpPage from '../SignUp'
 
 const App = () => (
-  
   <Router>
- <AuthUserContext.Consumer>
-    {
-      (authUser) =>
-      authUser ? (
-      authUser.bank === 'LF'?<GlobalStyleLF /> :authUser.bank === 'SB'? <GlobalStyleSB />:<GlobalStyle />):(<GlobalStyle />)
-    }
+    <AuthUserContext.Consumer>
+      {(authUser) =>
+        authUser ? (
+          authUser.bank === 'LF' ? (
+            <GlobalStyleLF />
+          ) : authUser.bank === 'SB' ? (
+            <GlobalStyleSB />
+          ) : (
+            <GlobalStyle />
+          )
+        ) : (
+          <GlobalStyle />
+        )
+      }
     </AuthUserContext.Consumer>
     <div>
-
-      <Navbar/>
+      <Navbar />
       <Switch>
         <Route exact path={ROUTES.LANDING} component={LandingPage} />
         <Route path={ROUTES.SIGN_UP} component={SignUpPage} />
@@ -37,14 +43,13 @@ const App = () => (
         <Route path={ROUTES.PASSWORD_FORGET} component={PasswordForgetPage} />
         <Route path={ROUTES.HOME} component={HomePage} />
         <Route path={ROUTES.ACCOUNT} component={AccountPage} />
-        <Route path={ROUTES.ADMIN} component={AdminPage} /> 
-        <Route path={ROUTES.CONTACT} component={ContactPage} /> 
-        <Route path={ROUTES.ABOUT} component={About} /> 
+        <Route path={ROUTES.ADMIN} component={AdminPage} />
+        <Route path={ROUTES.CONTACT} component={ContactPage} />
+        <Route path={ROUTES.ABOUT} component={About} />
         <Route component={Error} />
       </Switch>
     </div>
   </Router>
-);
+)
 
-export default withAuthentication(App);
-
+export default withAuthentication(App)
